e196baa7925a4a1761f6e947e9edbf7d402d2455
letyresina <leticiaresina@gmail.com>
2023-11-22 16:32:41 -0300

    Organizando sass

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import './Carousel.scss'
 
 const Carrossel = () => {
   return (
